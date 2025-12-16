d88e5c978644e5392268426148909a2a82c30acf
Symbat <symbataldamurat2@gmail.com>
2025-12-16 12:33:17 +0500

    oop1

--- src/ZooKeeper.java
+++ src/ZooKeeper.java
@@ -1,3 +1,18 @@
-void main() {
+public class ZooKeeper {
+    private String name;
+    private int experience;
 
+    public ZooKeeper (String name, int experience){
+        this.name=name;
+        this.experience=experience;
+    }
+    public String getName(){
+        return name;
+    }
+    public int getExperience(){
+        return experience;
+    }
+    public void displayZooKeeperInfo(){
+        System.out.println("Zookeeper: " + name + ", Experience: " + experience + "years ");
+    }
 }
