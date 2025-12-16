2d69eae6865eb641b4d25c352300b03c6827e906
Symbat <symbataldamurat2@gmail.com>
2025-12-16 15:03:37 +0500

    oop1

--- src/zoo.java
+++ src/zoo.java
@@ -1,3 +1,12 @@
-void main() {
-
+public class Zoo {
+    private String zooname;
+    public zoo (String zooname){
+        this.zooname=zooname;
+    }
+    public String getZooname (){
+        return zooname;
+    }
+    public String displayZooInfo(){
+        System.out.println("Zoo: " + zooname);
+    }
 }
