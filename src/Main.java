c8c428c4da5b88c6e9f1f88c178f9aa69cac5016
Symbat <symbataldamurat2@gmail.com>
2025-12-17 12:33:02 +0500

    oop1

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,19 @@
 public class Main {
     public static void main (String[] args){
-        Animal animal1=new Animal("Lion")
+        Animal animal1=new Animal("Lion", "predator",5);
+        Animal animal2=new Animal("Lion", "predator",5);
 
+        ZooKeeper keeper = new ZooKeeper("Sam", 10);
+        Zoo zoo= new Zoo(" Сentral zoo");
+
+        zoo.displayZooInfo();
+        animal1.displayAnimalInfo();
+        keeper.displayZooKeeperInfo();
+
+        if (animal1.getName().equals(animal2.getName())){
+            System.out.println("animals have the same name");
+        }else {
+            System.out.println("animals are different");
+        }
     }
 }
